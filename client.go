7839285a23a9c61dee37c91d38c5710c7d3c6a7f
Peltoche <dev@halium.fr>
2019-03-05 17:16:51 +0100

    Regroup the call logic inside the execRequest method

--- client.go
+++ client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
@@ -52,28 +54,13 @@ func (c *Client) GetSchemaByID(ctx context.Context, subjectID int) (string, erro
 		Schema string `json:"schema"`
 	}
 
-	// nolint
-	// The path cannot be invalid
-	path, _ := url.Parse(fmt.Sprintf("schemas/ids/%d", subjectID))
-
-	// nolint
-	// The request is always valid
-	req, _ := http.NewRequest("GET", c.baseURL.ResolveReference(path).String(), nil)
-	req.Header.Add("Accept", "application/json")
-
-	res, err := c.client.Do(req.WithContext(ctx))
-	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
-
-	err = parseResponseError(req, res)
+	rawBody, err := c.execRequest(ctx, "GET", fmt.Sprintf("schemas/ids/%d", subjectID), nil)
 	if err != nil {
 		return "", err
 	}
 
 	var resBody responseBody
-	err = json.NewDecoder(res.Body).Decode(&resBody)
+	err = json.Unmarshal(rawBody, &resBody)
 	if err != nil {
 		return "", fmt.Errorf("failed to decode the response: %s", err)
 	}
@@ -87,28 +74,13 @@ func (c *Client) GetSchemaByID(ctx context.Context, subjectID int) (string, erro
 func (c *Client) Subjects(ctx context.Context) (subjects []string, err error) {
 	type responseBody []string
 
-	// nolint
-	// The path cannot be invalid
-	path, _ := url.Parse("subjects")
-
-	// nolint
-	// The request is always valid
-	req, _ := http.NewRequest("GET", c.baseURL.ResolveReference(path).String(), nil)
-	req.Header.Add("Accept", "application/json")
-
-	res, err := c.client.Do(req.WithContext(ctx))
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	err = parseResponseError(req, res)
+	rawBody, err := c.execRequest(ctx, "GET", "subjects", nil)
 	if err != nil {
 		return nil, err
 	}
 
 	var resBody responseBody
-	err = json.NewDecoder(res.Body).Decode(&resBody)
+	err = json.Unmarshal(rawBody, &resBody)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode the response: %s", err)
 	}
@@ -122,29 +94,13 @@ func (c *Client) Subjects(ctx context.Context) (subjects []string, err error) {
 func (c *Client) Versions(ctx context.Context, subject string) (versions []int, err error) {
 	type responseBody []int
 
-	path, err := url.Parse(fmt.Sprintf("subjects/%s/versions", subject))
-	if err != nil {
-		return nil, err
-	}
-
-	// nolint
-	// The request is always valid
-	req, _ := http.NewRequest("GET", c.baseURL.ResolveReference(path).String(), nil)
-	req.Header.Add("Accept", "application/json")
-
-	res, err := c.client.Do(req.WithContext(ctx))
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	err = parseResponseError(req, res)
+	rawBody, err := c.execRequest(ctx, "GET", fmt.Sprintf("subjects/%s/versions", subject), nil)
 	if err != nil {
 		return nil, err
 	}
 
 	var resBody responseBody
-	err = json.NewDecoder(res.Body).Decode(&resBody)
+	err = json.Unmarshal(rawBody, &resBody)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode the response: %s", err)
 	}
@@ -160,14 +116,39 @@ func (c *Client) Versions(ctx context.Context, subject string) (versions []int, 
 func (c *Client) DeleteSubject(ctx context.Context, subject string) (versions []int, err error) {
 	type responseBody []int
 
-	path, err := url.Parse(fmt.Sprintf("subjects/%s", subject))
+	rawBody, err := c.execRequest(ctx, "GET", fmt.Sprintf("subjects/%s", subject), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resBody responseBody
+	err = json.Unmarshal(rawBody, &resBody)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode the response: %s", err)
+	}
+
+	return resBody, nil
+}
+
+// Execute the request and check for an error into the response.
+//
+// In case of succes it return the raw body.
+//
+// It return an error if:
+// - an error occure with the network
+// - an error occure with the remote api
+// - the request the params have an invalid
+// - the response have an invalid format
+// - the response is an error
+func (c *Client) execRequest(ctx context.Context, method string, rawPath string, body io.Reader) ([]byte, error) {
+	path, err := url.Parse(rawPath)
 	if err != nil {
 		return nil, err
 	}
 
 	// nolint
 	// The request is always valid
-	req, _ := http.NewRequest("DELETE", c.baseURL.ResolveReference(path).String(), nil)
+	req, _ := http.NewRequest(method, c.baseURL.ResolveReference(path).String(), nil)
 	req.Header.Add("Accept", "application/json")
 
 	res, err := c.client.Do(req.WithContext(ctx))
@@ -181,11 +162,10 @@ func (c *Client) DeleteSubject(ctx context.Context, subject string) (versions []
 		return nil, err
 	}
 
-	var resBody responseBody
-	err = json.NewDecoder(res.Body).Decode(&resBody)
+	rawBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode the response: %s", err)
+		return nil, err
 	}
 
-	return resBody, nil
+	return rawBody, nil
 }
